fd0296c72a0bd3af2bcd1e5f28487a4aa964c0ad
Vitaliy <vitalik.yakhimovich@mail.ru>
2024-02-22 00:17:44 +0300

    Implemented DAO removal

---
+++ src/notebook/model/repository/impl/UserRepository.java
@@ -0,0 +1,155 @@
+package notebook.model.repository.impl;
+
+//import notebook.model.dao.impl.FileOperation;
+
+import notebook.util.DBConnector;
+import notebook.util.UserValidator;
+import notebook.util.mapper.impl.UserMapper;
+import notebook.model.User;
+import notebook.model.repository.GBRepository;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+
+public class UserRepository implements GBRepository {
+    private final UserMapper mapper;
+
+    public UserRepository(String fileName) {
+        this.mapper = new UserMapper();
+    }
+
+    @Override
+    public List<User> findAll() {
+        List<String> lines = readAll();
+        List<User> users = new ArrayList<>();
+        for (String line : lines) {
+            users.add(mapper.toOutput(line));
+        }
+        return users;
+    }
+
+    public User createUser() {
+        String firstName = prompt("Имя: ");
+        String lastName = prompt("Фамилия: ");
+        String phone = prompt("Номер телефона: ");
+
+        UserValidator validator = new UserValidator();
+
+        return validator.validate(new User(firstName, lastName, phone));
+    }
+
+    private String prompt(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
+
+    @Override
+    public User create(User user) {
+        List<User> users = findAll();
+        long max = 0L;
+        for (User u : users) {
+            long id = u.getId();
+            if (max < id) {
+                max = id;
+            }
+        }
+        long next = max + 1;
+        user.setId(next);
+        users.add(user);
+        write(users);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> update(Long userId, User update) {
+        List<User> users = findAll();
+        User editUser = users.stream()
+                .filter(u -> u.getId()
+                        .equals(userId))
+                .findFirst().orElseThrow(() -> new RuntimeException("User not found"));
+        if (!update.getFirstName().isEmpty()) {
+            editUser.setFirstName(update.getFirstName());
+        }
+        if (!update.getLastName().isEmpty()) {
+            editUser.setLastName(update.getLastName());
+        }
+        if (!update.getPhone().isEmpty()) {
+            editUser.setPhone(update.getPhone());
+        }
+        write(users);
+        return Optional.of(update);
+    }
+
+    @Override
+    public boolean delete(Long userId) {
+        List<User> users = findAll();
+        User editUser = users.stream()
+                .filter(u -> u.getId()
+                        .equals(userId))
+                .findFirst().orElseThrow(() -> new RuntimeException("User not found"));
+        users.remove(editUser);
+        write(users);
+        return true;
+    }
+
+    private void write(List<User> users) {
+        List<String> lines = new ArrayList<>();
+        for (User u : users) {
+            lines.add(mapper.toInput(u));
+        }
+        saveAll(lines);
+    }
+
+    public List<String> readAll() {
+        List<String> lines = new ArrayList<>();
+        try {
+            File file = new File(DBConnector.DB_PATH);
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            if (line != null) {
+                lines.add(line);
+            }
+            while (line != null) {
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+                if (line != null) {
+                    lines.add(line);
+                }
+            }
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    @Override
+    public void saveAll(List<String> data) {
+        try (FileWriter writer = new FileWriter(DBConnector.DB_PATH, false)) {
+            for (String line : data) {
+                // запись всей строки
+                writer.write(line);
+                // запись по символам
+                writer.append('\n');
+            }
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
---
+++ src/notebook/view/UserView.java
@@ -0,0 +1,66 @@
+package notebook.view;
+
+import notebook.controller.UserController;
+import notebook.model.User;
+import notebook.util.Commands;
+import notebook.util.UserValidator;
+
+import java.util.Scanner;
+
+public class UserView {
+    private final UserController userController;
+
+    public UserView(UserController userController) {
+        this.userController = userController;
+    }
+
+    public void run(){
+        Commands com;
+
+        while (true) {
+            String command = prompt("Введите команду: ");
+            com = Commands.valueOf(command);
+            if (com == Commands.EXIT) return;
+            switch (com) {
+                case CREATE:
+                    User u = userController.createUser();
+                    userController.saveUser(u);
+                    break;
+                case READ:
+                    String id = prompt("Идентификатор пользователя: ");
+                    try {
+                        User user = userController.readUser(Long.parseLong(id));
+                        System.out.println(user);
+                        System.out.println();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case LIST:
+                    System.out.println(userController.readAll());
+                case UPDATE:
+                    String userId = prompt("Enter user id: ");
+                    userController.updateUser(userId, userController.createUser());
+                case DELETE:
+                    String userId2 = prompt("Enter user id: ");
+                    userController.deleteUser(userId2);
+            }
+        }
+    }
+
+    private String prompt(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
+//
+//    private User createUser() {
+//        String firstName = prompt("Имя: ");
+//        String lastName = prompt("Фамилия: ");
+//        String phone = prompt("Номер телефона: ");
+//
+//        UserValidator validator = new UserValidator();
+//
+//        return validator.validate(new User(firstName, lastName, phone));
+//    }
+}
